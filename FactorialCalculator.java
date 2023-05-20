34ec63c7a0947e089ee60170960f9ac0315fc33f
Utkarsh Rai <126332147+raiutkarsh02@users.noreply.github.com>
2023-05-21 00:23:41 +0530

    Add files via upload

---
+++ FactorialCalculator.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class FactorialCalculator extends Frame implements ActionListener {
+    private Label label;
+    private TextField textField;
+    private Button button;
+
+    public FactorialCalculator() {
+        super("Factorial Calculator");
+
+        label = new Label("Enter a number:");
+        textField = new TextField(10);
+        button = new Button("Calculate");
+        button.addActionListener(this);
+
+        setLayout(new FlowLayout());
+        add(label);
+        add(textField);
+        add(button);
+
+        pack();
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String input = textField.getText();
+        int num = Integer.parseInt(input);
+
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+        String result = "The factorial of the number you entered is: " + factorial;
+        Dialog dialog = new Dialog(this, "Result", true);
+        Label label = new Label(result);
+        dialog.add(label);
+        dialog.pack();
+        dialog.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FactorialCalculator();
+    }
+}
