75bde091f00ae9e9222ec900d53252e2336aa567
raiutkarsh02 <126332147+raiutkarsh02@users.noreply.github.com>
2023-05-21 00:02:27 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,12 @@
+public class Second {
+    public static void main(String [] args){
+
+            System.out.println("Hello VIT.....");
+
+            for(int i=0;i<100;i++){
+                System.out.println(i);
+            }
+        }
+
+    }
+
