4eb97f55dc5d4fc24060b8be7eac4a99bf7d0a09
raiutkarsh02 <126332147+raiutkarsh02@users.noreply.github.com>
2023-05-21 00:05:35 +0530

    Add files via upload

---
+++ Awtprogram1.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+public class Awtprogram1{
+    public Awtprogram1(){
+        Frame f= new Frame();
+        Label l1 = new Label("Hello VIT");
+        f.add(l1);
+        f.setVisible(true);
+
+    }
+
+public static void main(String[] args){
+    Awtprogram1 awt= new Awtprogram1();
+}
+}
